b051baa2dca9d0cc002f6e69f559f362b2b9d8a7
yektasharifpour <154242506+yektasharifpour@users.noreply.github.com>
2025-12-14 17:23:18 +0330

    add player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speedForward = 5f;
+    [SerializeField] private float speedSide = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float moveForward = Input.GetAxis("Vertical") * speedForward * Time.deltaTime;
+        float moveSide = Input.GetAxis("Horizontal") * speedSide * Time.deltaTime;
+
+        transform.Translate(moveSide,0 , moveForward);
+
     }
 }
